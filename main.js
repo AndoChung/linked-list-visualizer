a78db6d7dd22b03348e6bfc4a0305962bd23f664
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2023-01-14 11:48:02 -0800

    push function is functional
    more style is needed

--- main.js
+++ main.js
@@ -6,26 +6,48 @@ addNodeButton.addEventListener("click", pushNode);
 
 
 function pushNode() {
+    /* Pushes a new node with a value specified by the user */
+
+    // Change the previous node next to the new node value
+    const nodeValue = userInput.value;
+    const lastNodeNextValue = linkedList.lastElementChild.lastElementChild.firstElementChild;
+    lastNodeNextValue.innerText = nodeValue;
+
+    // Create a new node
     const newNode = document.createElement("div");
     newNode.className = "node";
 
+    // specifies the index of the new node
     const lastNodeIndex = linkedList.lastElementChild.firstElementChild.innerText;
     const nodeIndex = document.createElement("div");
     nodeIndex.innerText = Number(lastNodeIndex) + 1;
     nodeIndex.className = "node-index";
     newNode.appendChild(nodeIndex);
 
+    // creates the body of the new node
     const nodeBody = document.createElement("div");
     nodeBody.className = "node-body";
     newNode.appendChild(nodeBody);
 
-    const nodeValue = userInput.value;
-
+    // Show the new node value 
     const nodeValueText = document.createElement("div");
     nodeValueText.className = "node-value-text";
+    nodeValueText.innerText = "Value: "
     newNode.appendChild(nodeValueText);
-    const trueNodeValue = document.createElement("div");
-    trueNodeValue.className = 
+    const trueNodeValue = document.createElement("span");
+    trueNodeValue.className = "true-node-value";
+    trueNodeValue.innerText = nodeValue;
+    nodeValueText.appendChild(trueNodeValue);
+    
+    // initialize node next as null
+    const nodeNextText = document.createElement("div");
+    nodeNextText.className = "node-next-text";
+    nodeNextText.innerText = "Next: "
+    newNode.appendChild(nodeNextText);
+    const trueNodeNext = document.createElement("span");
+    trueNodeNext.className = "true-node-next";
+    trueNodeNext.innerText = "null";
+    nodeNextText.appendChild(trueNodeNext);
 
 
 
