2a83c8f8b355f7dd67bd79a1b105f87933ce207c
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2023-01-14 01:20:02 -0800

    working add node

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 const addNodeButton = document.getElementById("add-node-button");
-const firstNode = document.getElementById("first-node");
 const linkedList = document.getElementById("linked-list");
-
+const userInput = document.getElementById("input-node-value");
 
 addNodeButton.addEventListener("click", pushNode);
 
 
 function pushNode() {
-    const newDiv = document.createElement("div");
-    newDiv.className = "node-body";
-    linkedList.appendChild(newDiv);
+    const newNode = document.createElement("div");
+    newNode.className = "node";
+
+    const lastNodeIndex = linkedList.lastElementChild.firstElementChild.innerText;
+    const nodeIndex = document.createElement("div");
+    nodeIndex.innerText = Number(lastNodeIndex) + 1;
+    nodeIndex.className = "node-index";
+    newNode.appendChild(nodeIndex);
+
+    const nodeBody = document.createElement("div");
+    nodeBody.className = "node-body";
+    newNode.appendChild(nodeBody);
+
+    const nodeValue = userInput.value;
+
+    const nodeValueText = document.createElement("div");
+    nodeValueText.className = "node-value-text";
+    newNode.appendChild(nodeValueText);
+    const trueNodeValue = document.createElement("div");
+    trueNodeValue.className = 
+
+
+
+    linkedList.appendChild(newNode);
 }
+
